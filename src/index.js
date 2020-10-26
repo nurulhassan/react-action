723b615b5a1024ff11792ef565cf8f41fb6d221c
unknown <kumarnaveen.ce20@gmail.com>
2020-10-26 11:02:48 +0530

    jsx expression added

--- src/index.js
+++ src/index.js
@@ -1,18 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const name = "Nurul";
-const element = <h1>Hello, {name}</h1>;
+function name(user){
+  return user.first+" "+user.last;
+}
+
+const user = {
+  first:'Nurul',
+  last:'Hassan'
+};
+
+const element = (
+<h1>Hello,Mr.{name(user)}</h1>
+);
 
 ReactDOM.render(
   element,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
