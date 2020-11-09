93f2478aaf089c04ab92eaf0ba5f348b182073c8
unknown <kumarnaveen.ce20@gmail.com>
2020-11-09 11:24:04 +0530

    Store and disptach done

--- src/redux-demo/index.js
+++ src/redux-demo/index.js
@@ -1,7 +1,7 @@
 const redux=require('redux');
-const store=redux.createStore;
+const createStore=redux.createStore;
 
-const Buy_Book="Buy Book";
+const Buy_Book="Buy_Book";
 
 const initialState={
     numberOfBooks:10
@@ -13,3 +13,25 @@ function buyBook(){
         info:"My First edux Code"
     }
 }
+
+const Reducer = (state=initialState,action) =>{
+    
+    switch(action.type){
+        case "Buy_Book":return{
+            ...state,
+            numberOfBooks:state.numberOfBooks-1
+        }
+        
+        default: return state;
+    }
+    
+}
+
+const store=createStore(Reducer);
+console.log("Initial State",store.getState());
+const unsubscribe=store.subscribe(()=>{console.log('Updated State Value', store.getState())});
+store.dispatch(buyBook());
+store.dispatch(buyBook());
+store.dispatch(buyBook());
+store.dispatch(buyBook());
+unsubscribe();
